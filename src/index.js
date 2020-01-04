c8a64baebaf95afc096127f4e2406f9e4f50de45
quantum.v.rl <quantum.v.rl@gmail.com>
2020-01-04 10:00:03 -0800

    "First major commit"

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'typeface-lobster';
+import 'typeface-open-sans';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
