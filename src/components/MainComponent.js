9fcd95ea6bdf09f5afefcdc06272818b108b58f2
quantum.v.rl <quantum.v.rl@gmail.com>
2020-01-08 23:20:06 -0800

    git commit FooterComponent.js

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import { CAMPSITES } from '../shared/campsites';
 
 class Main extends Component {
@@ -20,9 +21,10 @@ class Main extends Component {
     render() {
         return (
             <div>
-                <Header></Header>
+                <Header />
                 <Directory campsites={this.state.campsites} onClick={(campsiteId) => this.onCampsiteSelect(campsiteId)}/>
                 <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]}/>
+                <Footer />
             </div>
         );
     }
