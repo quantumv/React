e025f2555982775a170930c7242251d207a94275
quantum.v.rl <quantum.v.rl@gmail.com>
2020-01-23 20:58:27 -0800

    git commit fix 1.23.20@2058

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 
 const maxLength = len => val => !val || val.length <= len
@@ -102,13 +103,19 @@ class CommentForm extends Component {
 function RenderCampsite({campsite}) {
     return (
         <div className="col-md-5 m-1">
+        <FadeTransform
+            in
+            transformProps={{
+                exitTransform: 'scale(0.5) translateY(-50%)'
+            }}>
             <Card>
                 <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
                 <CardBody>
                     <CardText>{campsite.description}</CardText>
                 </CardBody>
             </Card>
-        </div>
+        </FadeTransform>
+    </div>
     );
 }
 
@@ -117,15 +124,19 @@ function RenderComments({comments, postComment, campsiteId}) {
         return (
             <div className="col-md-5 m1">
                 <h4>Comments</h4>
+                <Stagger in>
                 {comments.map(comment => {
                     return(
-                        <div key={comment.id}>
-                            <p>{comment.text}<br />
-                                --{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                            </p> 
-                        </div>
+                        <Fade in key={comment.id}>
+                            <div>
+                                <p>{comment.text}<br />
+                                    --{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                                </p> 
+                            </div>
+                        </Fade>
                     );
                 })}
+                </Stagger>
                 <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         );
